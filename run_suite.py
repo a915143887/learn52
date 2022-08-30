bdad6c506dc054f691b39b9bdd26eced78f1cc8b
lulu <a915143887@163.com>
2022-08-30 18:58:13 +0800

    更新04

--- run_suite.py
+++ run_suite.py
@@ -1,13 +1,19 @@
+#导包
 import unittest
 from tools.HTMLTestRunner_PY3 import HTMLTestRunner
 from scripts.test_mobil import mobile
 
+#封装测试套件
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(mobile))
 
+#指定报告存放位置
 report = "./report/report.html"
 
+#打开文件流
 with open(report, "wb") as e:
+    #创建运行器
     runner = HTMLTestRunner(e, title="mobile测试报告")
+    #运行
     runner.run(suite)
 
