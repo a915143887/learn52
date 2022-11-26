51aafc1764f86198c5684827b39421f77a9bb5a8
lulu <a915143887@163.com>
2022-11-26 20:43:17 +0800

    更新06

--- run_suite.py
+++ run_suite.py
@@ -14,6 +14,6 @@ report = "./report/report.html"
 #打开文件流
 with open(report, "wb") as e:
     #创建运行器
-    runner = HTMLTestRunner(e, title="mobile测试报告")
+    runner = HTMLTestRunner(e, title="mobile报告")
     #运行
     runner.run(suite)
